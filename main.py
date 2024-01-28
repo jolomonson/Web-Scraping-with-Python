44b642a24aceb63dfae378f472993a4257012158
John Solomon <jolomonson@gmail.com>
2024-01-28 14:11:43 +0100

    Use bs4 package to display pretty HTML content

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from bs4 import BeautifulSoup
 # Read html file and print existing content
 with open('home.html', 'r') as html_file:
     content = html_file.read()
-    print(content)
+    # Use Beautiful soup to prettify html content 
+    soup = BeautifulSoup(content, 'lxml')
+    print(soup.prettify())
