a2aca9d9194d92d59c4b8f291febd91dc45a0014
John Solomon <jolomonson@gmail.com>
2024-01-28 22:24:28 +0100

    Web scraping from jumia.com.ng

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 from bs4 import BeautifulSoup
+import requests
 
-# Read html file and print existing content
-with open('home.html', 'r') as html_file:
-    content = html_file.read()
-    # Use Beautiful soup to render html content 
-    soup = BeautifulSoup(content, 'lxml')
-    # Find element by div tag and class attribute
-    course_cards = soup.find_all('div', class_='card')
-
-    # Looping through each course card for course details
-    for course in course_cards:
-        course_name = course.h5.text
-        course_description = course.p.text
-        course_price = course.a.text.split()[-1]
-
-        print(course_name,'-',course_description,'It costs',course_price)
+#  Webscraping from jumia.com.ng
+html_text = requests.get('https://www.jumia.com.ng/catalog/?q=sneakers+for+men').text
+print(html_text)
