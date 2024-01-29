c746206eac62f5f23b05b4f6b34b8e349a68a83b
John Solomon <jolomonson@gmail.com>
2024-01-29 03:38:22 +0100

    Scraped 'sneaker' product details from jumia.com.ng

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ import requests
 html_text = requests.get('https://www.jumia.com.ng/catalog/?q=sneakers+for+men').text
 soup = BeautifulSoup(html_text, 'lxml')
 
-sneakers = soup.find('article', class_='prd _fb col c-prd')
-sneaker_name = sneakers.find('h3', class_='name').text
+sneakers = soup.find_all('article', class_='prd _fb col c-prd')
+for sneaker in sneakers:
+    sneaker_name = sneaker.find('h3', class_='name').text
+    sneaker_price = sneaker.find('div', class_='prc').text
+    #sneaker_discount = sneaker.find('div', class_='bdg _dsct _sm').text
+    sneaker_rating = sneaker.find('div', class_='stars _s').text
 
-print(sneaker_name)
+    print("Name: ",sneaker_name)
+    print("Price: ",sneaker_price)
+    print("Rating: ",sneaker_rating)
